eb86cb2a68ccd0edcc6d239c61a1cf7ea0b9ba50
Wenrui <58852708+wenruiq@users.noreply.github.com>
2020-09-20 11:56:07 +0800

    added redux store and reselect package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import './scss/App.scss';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
+
+
 
 class App extends React.Component {
 	render() {
