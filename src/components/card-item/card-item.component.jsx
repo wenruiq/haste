55406d8de8e044d6e033b06ac80b0394393d37bc
Wenrui <58852708+wenruiq@users.noreply.github.com>
2020-09-28 17:39:40 +0800

    changed bookmark icon to only display when user is logged in

--- src/components/card-item/card-item.component.jsx
+++ src/components/card-item/card-item.component.jsx
@@ -10,104 +10,100 @@ import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 // *import BookmarkIcon from '@material-ui/icons/Bookmark';
 
 class CardItem extends React.Component {
-	handleSave = () => {
-		const testProduct = {
-			id: '3j1ddui31m',
-			name: 'Big Bag',
-			price: '$13.37',
-			description: "big bag 300kg you can't handle for sure",
-		};
-		const { addSavedStartAsync, currentUser } = this.props;
-		console.log(currentUser.id);
-		addSavedStartAsync(currentUser.id, testProduct);
-	};
+  handleSave = () => {
+    const {
+      name,
+      description,
+      price,
+      image,
+      url,
+      addSavedStartAsync,
+      currentUser,
+    } = this.props;
 
-	render() {
-		return (
-			<div className='card-item-container'>
-				<div className='card-item'>
-					<div className='brand-and-save-container'>
-						{/* //!Brand and Save Icon Container - 10% */}
-						<div className='brand-container'>
-							<img
-								src={require('../../assets/brands/shopee-logo.png')}
-								alt='placeholder-alt-change-later'
-							/>
-						</div>
-						<BookmarkBorderIcon className='save-item-icon' onClick={() => this.handleSave()} />
-					</div>
+    const productToBeAdded = {
+      name,
+      description,
+      price,
+      image,
+      url,
+    };
+    addSavedStartAsync(currentUser.id, productToBeAdded);
+  };
 
-					{/* Item Pic container - 50% */}
-					<div className='item-pic-container'>
-						<a href='/'>
-							<img
-								// src={require('../../assets/test-item-image.jpg')}
-								src={this.props.image}
-								alt='placeholder-alt-change-later'
-							></img>
-						</a>
-					</div>
+  render() {
+    const { name, description, price, image, url, currentUser } = this.props;
+    return (
+      <div className="card-item-container">
+        <div className="card-item">
+          <div className="brand-and-save-container">
+            <div className="brand-container">
+              <img
+                src={require('../../assets/brands/shopee-logo.png')}
+                alt="placeholder-alt-change-later"
+              />
+            </div>
+            {currentUser ? (
+              <BookmarkBorderIcon
+                className="save-item-icon"
+                onClick={() => this.handleSave()}
+              />
+            ) : (
+              ''
+            )}
+          </div>
+          <div className="item-pic-container">
+            <a href="/">
+              <img src={image} alt="placeholder-alt-change-later"></img>
+            </a>
+          </div>
+          <div className="item-pic-buttons">
+            <a
+              className="button"
+              href="https://www.google.com.sg"
+              target="_blank"
+              rel="noopener noreferrer"
+              alt="placeholder-alt"
+            >
+              Find Similar
+            </a>
+            <a
+              className="button"
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              alt="placeholder-alt"
+            >
+              View Details
+            </a>
+          </div>
+          <div className="item-details-container">
+            <div className="item-description-container">
+              <div className="item-description-primary">
+                {name || 'Gray Polo Shirt'}
+              </div>
+              <div className="item-description-secondary">
+                {description ||
+                  'Light gray shirt made with 100% cotton in China'}
+              </div>
+            </div>
 
-					<div className='item-pic-buttons'>
-						{/* //! Change to Link & API link respectively in the future*/}
-						<a
-							className='button'
-							href='https://www.google.com.sg'
-							target='_blank'
-							rel='noopener noreferrer'
-							alt='placeholder-alt'
-						>
-							Find Similar
-						</a>
-						<a
-							className='button'
-							//! this has been changed into search result URL
-							// href='https://www.google.com.sg'
-							href={this.props.url}
-							target='_blank'
-							rel='noopener noreferrer'
-							alt='placeholder-alt'
-						>
-							View Details
-						</a>
-					</div>
-
-					{/* Item Details Container - 40% */}
-					<div className='item-details-container'>
-						<div className='item-description-container'>
-							{/* //! This has been changed to search result name */}
-
-							<div className='item-description-primary'>{this.props.name || 'Gray Polo Shirt'}</div>
-							{/* <div className='item-description-primary'>Gray Polo Shirt</div> */}
-							{/* <div className='item-description-primary'>{this.props.name}</div> */}
-							<div className='item-description-secondary'>
-								{/* //! This has been changed to search result description */}
-								{this.props.description || 'Light gray shirt made with 100% cotton in China'}
-								{/* {this.props.description} */}
-								{/* Light gray shirt made with 100% cotton in China */}
-							</div>
-						</div>
-						{/* //! This has been changed to search result price */}
-
-						<div className='item-price'>{this.props.price || '$35.00'}</div>
-						{/* <div className='item-price'>{this.props.price}</div> */}
-						{/* <div className='item-price'>$35.00</div> */}
-					</div>
-
-					{/* <BookmarkIcon /> */}
-				</div>
-			</div>
-		);
-	}
+            <div className="item-price">{price || '$35.00'}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-	addSavedStartAsync: (userID, product) => dispatch(addSavedStartAsync(userID, product)),
+const mapDispatchToProps = dispatch => ({
+  addSavedStartAsync: (userID, product) =>
+    dispatch(addSavedStartAsync(userID, product)),
 });
 
-const mapStateToProps = (state) =>
-	createStructuredSelector({
-		currentUser: selectCurrentUser,
-	});
+const mapStateToProps = state =>
+  createStructuredSelector({
+    currentUser: selectCurrentUser,
+  });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CardItem);
--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -6,14 +6,12 @@ import DisplayBoxHeading from '../../components/display-box-heading/display-box-
 
 import FourItemDisplayBox from '../../components/four-item-display-box/four-item-display-box.component';
 
-import TestPage from '../test-page/test-page.component';
 
 const HomePage = () => (
 	// Homepage make a fetch for 4 random items for popular & you might like
 	<div className='home-page-container'>
 		<DisplayBoxHeading title='You Might Like' />
 		<FourItemDisplayBox />
-		<TestPage />
 		<DisplayBoxHeading title='Popular' />
 		<FourItemDisplayBox />
 	</div>
