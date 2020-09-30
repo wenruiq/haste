f7b35a27ab37f7abc901a8e090361834a4f32a0a
lifuhuang97 <54570187+lifuhuang97@users.noreply.github.com>
2020-09-30 11:38:17 +0800

    updated build for the app

---
+++ build/precache-manifest.012536006b3b62a2d4b9ae180c427400.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3fe705568f65673c6dd25fe367761a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0880665ddc572e147aa",
+    "url": "/static/css/main.d2e73ac8.chunk.css"
+  },
+  {
+    "revision": "21a86cd51cb239c00aa8",
+    "url": "/static/js/2.06ad9c92.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.06ad9c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0880665ddc572e147aa",
+    "url": "/static/js/main.0d53a563.chunk.js"
+  },
+  {
+    "revision": "2b7316b6764d6162f253",
+    "url": "/static/js/runtime-main.caa1cc7b.js"
+  },
+  {
+    "revision": "4ca1b50f8cf964d1b9ecedf009724842",
+    "url": "/static/media/shopee-logo.4ca1b50f.png"
+  }
+]);
