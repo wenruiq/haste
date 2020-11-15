010e0310809822913c8f9ceaf96672ce5e81f459
wenruiq <wenrui119@DESKTOP-52NMHUE.localdomain>
2020-11-15 17:50:57 +0800

    improved css and fixed minor bug on card

--- src/components/card-item/card-item.component.jsx
+++ src/components/card-item/card-item.component.jsx
@@ -5,7 +5,10 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectSavedItems } from '../../redux/saved/saved.selectors';
-import { addSavedStartAsync, deleteSavedStartAsync } from '../../redux/saved/saved.actions';
+import {
+  addSavedStartAsync,
+  deleteSavedStartAsync,
+} from '../../redux/saved/saved.actions';
 
 import { updateFindSimilarQuery } from '../../redux/search/search.actions';
 
@@ -19,162 +22,171 @@ import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import BookmarkFilledIcon from '@material-ui/icons/Bookmark';
 
 class CardItem extends React.Component {
-	handleSave = () => {
-		const {
-			id,
-			name,
-			shortenedName,
-			description,
-			shortenedDescription,
-			price,
-			image,
-			url,
-			keyword,
-			addSavedStartAsync,
-			currentUser,
-		} = this.props;
+  handleSave = () => {
+    const {
+      id,
+      name,
+      shortenedName,
+      description,
+      shortenedDescription,
+      price,
+      image,
+      url,
+      keyword,
+      addSavedStartAsync,
+      currentUser,
+    } = this.props;
 
-		const productToBeAdded = {
-			id,
-			name,
-			shortenedName,
-			description,
-			shortenedDescription,
-			price,
-			image,
-			url,
-			keyword,
-		};
-		addSavedStartAsync(currentUser.id, productToBeAdded);
-	};
+    const productToBeAdded = {
+      id,
+      name,
+      shortenedName,
+      description,
+      shortenedDescription,
+      price,
+      image,
+      url,
+      keyword,
+    };
 
-	handleDelete = () => {
-		const { id, currentUser, deleteSavedStartAsync } = this.props;
-		deleteSavedStartAsync(currentUser.id, id);
-	};
+    if (!!productToBeAdded.id) {
+      addSavedStartAsync(currentUser.id, productToBeAdded);
+    }
+  };
 
-	handleFindSimilar = () => {
-		const {
-			id,
-			name,
-			shortenedName,
-			description,
-			shortenedDescription,
-			price,
-			image,
-			url,
-			keyword,
-			addSavedStartAsync,
-			currentUser,
-		} = this.props;
+  handleDelete = () => {
+    const { id, currentUser, deleteSavedStartAsync } = this.props;
+    if (!!id) {
+      deleteSavedStartAsync(currentUser.id, id);
+    }
+  };
 
-		const originalObj = {
-			id,
-			name,
-			shortenedName,
-			description,
-			shortenedDescription,
-			price,
-			image,
-			url,
-			keyword,
-			addSavedStartAsync,
-			currentUser,
-		};
+  handleFindSimilar = () => {
+    const {
+      id,
+      name,
+      shortenedName,
+      description,
+      shortenedDescription,
+      price,
+      image,
+      url,
+      keyword,
+      addSavedStartAsync,
+      currentUser,
+    } = this.props;
 
-		updateFindSimilarQuery(originalObj);
-	};
+    const originalObj = {
+      id,
+      name,
+      shortenedName,
+      description,
+      shortenedDescription,
+      price,
+      image,
+      url,
+      keyword,
+      addSavedStartAsync,
+      currentUser,
+    };
 
-	render() {
-		const {
-			id,
-			shortenedName,
-			name,
-			shortenedDescription,
-			description,
-			price,
-			image,
-			url,
-			source,
-			currentUser,
-			savedItems,
-		} = this.props;
-		const isSaved = !!savedItems.find((item) => item.id === id);
+    updateFindSimilarQuery(originalObj);
+  };
 
-		return (
-			<div className='card-item-container'>
-				<div className='card-item'>
-					<div className='brand-and-save-container'>
-						<div className='brand-container'>
-							<img src={source === 'ebay' ? eBay : BestBuy} alt='product-img' />
-						</div>
-						{currentUser ? (
-							isSaved ? (
-								<BookmarkFilledIcon
-									className='save-item-icon'
-									onClick={() => this.handleDelete()}
-								/>
-							) : (
-								<BookmarkBorderIcon className='save-item-icon' onClick={() => this.handleSave()} />
-							)
-						) : (
-							''
-						)}
-					</div>
-					<div className='item-pic-container'>
-						<a href='/'>
-							<img src={image} alt='placeholder-alt-change-later'></img>
-						</a>
-					</div>
-					<div className='item-pic-buttons'>
-						<Link
-							className='button'
-							to={`/search/${shortenedName}`}
-							rel='noopener noreferrer'
-							alt='placeholder-alt'
-							onClick={() => this.handleFindSimilar()}
-						>
-							Find Similar
-						</Link>
-						<a
-							className='button'
-							href={url}
-							target='_blank'
-							rel='noopener noreferrer'
-							alt='placeholder-alt'
-						>
-							View Details
-						</a>
-					</div>
-					<div className='item-details-container'>
-						<div className='item-description-container'>
-							<div className='item-description-primary'>
-								{(shortenedName ? shortenedName : name) || 'Gray Polo Shirt'}
-							</div>
-							<div className='item-description-secondary'>
-								{(shortenedDescription ? shortenedDescription : description) ||
-									'Light gray shirt made with 100% cotton in China'}
-							</div>
-						</div>
+  render() {
+    const {
+      id,
+      shortenedName,
+      name,
+      shortenedDescription,
+      description,
+      price,
+      image,
+      url,
+      source,
+      currentUser,
+      savedItems,
+    } = this.props;
+    const isSaved = !!savedItems.find(item => item.id === id);
 
-						<div className='item-price'>{'$' + price || '$35.00'}</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="card-item-container">
+        <div className="card-item">
+          <div className="brand-and-save-container">
+            <div className="brand-container">
+              <img src={source === 'ebay' ? eBay : BestBuy} alt="product-img" />
+            </div>
+            {currentUser ? (
+              isSaved ? (
+                <BookmarkFilledIcon
+                  className="save-item-icon"
+                  onClick={() => this.handleDelete()}
+                />
+              ) : (
+                <BookmarkBorderIcon
+                  className="save-item-icon"
+                  onClick={() => this.handleSave()}
+                />
+              )
+            ) : (
+              ''
+            )}
+          </div>
+          <div className="item-pic-container">
+            <img src={image} alt="placeholder-alt-change-later"></img>
+          </div>
+          <div className="item-pic-buttons">
+            <Link
+              className="button"
+              to={`/search/${shortenedName}`}
+              rel="noopener noreferrer"
+              alt="placeholder-alt"
+              onClick={() => this.handleFindSimilar()}
+            >
+              Find Similar
+            </Link>
+            <a
+              className="button"
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              alt="placeholder-alt"
+            >
+              View Details
+            </a>
+          </div>
+          <div className="item-details-container">
+            <div className="item-description-container">
+              <div className="item-description-primary">
+                {(shortenedName ? shortenedName : name) || 'Gray Polo Shirt'}
+              </div>
+              <div className="item-description-secondary">
+                {(shortenedDescription ? shortenedDescription : description) ||
+                  'Light gray shirt made with 100% cotton in China'}
+              </div>
+            </div>
+
+            <div className="item-price">{'$' + price || '$35.00'}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-	addSavedStartAsync: (userID, product) => dispatch(addSavedStartAsync(userID, product)),
-	deleteSavedStartAsync: (userID, productID) => dispatch(deleteSavedStartAsync(userID, productID)),
-	updateFindSimilarQuery: (originalObj) => dispatch(updateFindSimilarQuery(originalObj)),
+const mapDispatchToProps = dispatch => ({
+  addSavedStartAsync: (userID, product) =>
+    dispatch(addSavedStartAsync(userID, product)),
+  deleteSavedStartAsync: (userID, productID) =>
+    dispatch(deleteSavedStartAsync(userID, productID)),
+  updateFindSimilarQuery: originalObj =>
+    dispatch(updateFindSimilarQuery(originalObj)),
 });
 
-const mapStateToProps = (state) =>
-	createStructuredSelector({
-		savedItems: selectSavedItems,
-		currentUser: selectCurrentUser,
-	});
+const mapStateToProps = state =>
+  createStructuredSelector({
+    savedItems: selectSavedItems,
+    currentUser: selectCurrentUser,
+  });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CardItem);
