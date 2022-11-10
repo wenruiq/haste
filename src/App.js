b5ad8b15d5f0df46726dd45165bb7c7d574f5bbf
Synera <54570187+lifuhuang97@users.noreply.github.com>
2022-11-11 00:22:13 +0800

    fixed syntax of added iife

--- src/App.js
+++ src/App.js
@@ -23,14 +23,14 @@ import { selectSuggestConsent } from './redux/suggest/suggest.selectors';
 
 //* Added to enable cors-anywhere when new user visits the demo
 (() => {
-	var cors_api_host = 'cors-anywhere.herokuapp.com';
-	var cors_api_url = 'https://' + cors_api_host + '/';
-	var slice = [].slice;
-	var origin = window.location.protocol + '//' + window.location.host;
-	var open = XMLHttpRequest.prototype.open;
+	let cors_api_host = 'cors-anywhere.herokuapp.com';
+	let cors_api_url = 'https://' + cors_api_host + '/';
+	let slice = [].slice;
+	let origin = window.location.protocol + '//' + window.location.host;
+	let open = XMLHttpRequest.prototype.open;
 	XMLHttpRequest.prototype.open = function() {
-			var args = slice.call(arguments);
-			var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+			let args = slice.call(arguments);
+			let targetOrigin = "/^https?:\/\/([^\/]+)/i.exec(args[1])";
 			if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
 					targetOrigin[1] !== cors_api_host) {
 					args[1] = cors_api_url + args[1];
