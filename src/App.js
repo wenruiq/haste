28d88f40ea7ade9e0e687ffe933fbefb9442af15
Synera <54570187+lifuhuang97@users.noreply.github.com>
2022-11-11 00:19:53 +0800

    moved cors IIFE to app

--- src/App.js
+++ src/App.js
@@ -21,6 +21,24 @@ import { selectCurrentUser } from './redux/user/user.selectors';
 import { fetchSavedStartAsync, setSaved } from './redux/saved/saved.actions';
 import { selectSuggestConsent } from './redux/suggest/suggest.selectors';
 
+//* Added to enable cors-anywhere when new user visits the demo
+(() => {
+	var cors_api_host = 'cors-anywhere.herokuapp.com';
+	var cors_api_url = 'https://' + cors_api_host + '/';
+	var slice = [].slice;
+	var origin = window.location.protocol + '//' + window.location.host;
+	var open = XMLHttpRequest.prototype.open;
+	XMLHttpRequest.prototype.open = function() {
+			var args = slice.call(arguments);
+			var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+			if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+					targetOrigin[1] !== cors_api_host) {
+					args[1] = cors_api_url + args[1];
+			}
+			return open.apply(this, args);
+	};
+})();
+
 // *Class based component
 class App extends React.Component {
 	unsubscribeFromAuth = null;
@@ -32,6 +50,8 @@ class App extends React.Component {
 			setCurrentUser,
 			fetchSavedStartAsync,
 		} = this.props;
+
+
 
 		// *Subscribe to auth
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
